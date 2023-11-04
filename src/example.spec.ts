d3899c25749fa0cb59ff984dd45f0726e6aa1027
Patryk Lizoń <lizon.patryk@gmail.com>
2023-11-04 16:07:35 +0100

    refactor: Make running test for multiple implementation simpler

--- src/example.spec.ts
+++ src/example.spec.ts
@@ -1,14 +1,14 @@
 import { describe, expect, it } from "vitest";
 
-import { flatten } from "./example";
+import * as all from "./example";
 
-const fns = [flatten];
-const solutions = fns.map((cb, idx) => [idx, cb] as const);
+const fns = Object.values(all);
+const solutions = fns.map((cb) => [cb.name, cb] as const);
 
 type Fn = (typeof fns)[0];
 type InputExpectedPairs = [input: Parameters<Fn>, expected: ReturnType<Fn>];
 
-describe.each(solutions)("solution %s", (_, fn) => {
+describe.each(solutions)("%s", (_, fn) => {
   it.each<InputExpectedPairs>([
     [[[1, 2, 3, 4, 5, 6, 7]], [1, 2, 3, 4, 5, 6, 7]],
     [[[1, [2, [3, [4, [5, [6, [7]]]]]]]], [1, 2, 3, 4, 5, 6, 7]],
