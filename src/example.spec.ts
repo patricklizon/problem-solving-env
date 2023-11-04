5ab1eec7653e2c412b4a64efd76224ff01734914
Patryk Lizoń <lizon.patryk@gmail.com>
2023-11-04 15:52:54 +0100

    refactor: Use native each method for test

--- src/example.spec.ts
+++ src/example.spec.ts
@@ -1,26 +1,27 @@
-import { describe, it } from "vitest";
+import { describe, expect, it } from "vitest";
 
 import { flatten } from "./example";
-import { runTestCases, type TestCases } from "./utils";
 
-describe("#" + flatten.name, () => {
-  it("flattens non empty array", () => {
-    const testCases: TestCases<typeof flatten> = [
-      [[[1, 2, 3, 4, 5, 6, 7]], [1, 2, 3, 4, 5, 6, 7]],
-      [[[1, [2, [3, [4, [5, [6, [7]]]]]]]], [1, 2, 3, 4, 5, 6, 7]],
-      [[[[], [[[1]]], [[]], [[[], [2]]]]], [1, 2]],
-    ];
+const fns = [flatten];
+const solutions = fns.map((cb, idx) => [idx, cb] as const);
 
-    runTestCases(flatten, testCases);
+type Fn = (typeof fns)[0];
+type InputExpectedPairs = [input: Parameters<Fn>, expected: ReturnType<Fn>];
+
+describe.each(solutions)("solution %s", (_, fn) => {
+  it.each<InputExpectedPairs>([
+    [[[1, 2, 3, 4, 5, 6, 7]], [1, 2, 3, 4, 5, 6, 7]],
+    [[[1, [2, [3, [4, [5, [6, [7]]]]]]]], [1, 2, 3, 4, 5, 6, 7]],
+    [[[[], [[[1]]], [[]], [[[], [2]]]]], [1, 2]],
+  ])("flattens non empty array", (args, right) => {
+    expect(fn(...args)).to.deep.equal(right);
   });
 
-  it("flattens empty array", () => {
-    const testCases: TestCases<typeof flatten> = [
-      [[[]], []],
-      [[[[[[[[[]]]]]]]], []],
-      [[[[], [[[]]], [[]], [[[], []]]]], []],
-    ];
-
-    runTestCases(flatten, testCases);
+  it.each<InputExpectedPairs>([
+    [[[]], []],
+    [[[[[[[[[]]]]]]]], []],
+    [[[[], [[[]]], [[]], [[[], []]]]], []],
+  ])("flattens empty array", (args, right) => {
+    expect(fn(...args)).to.deep.equal(right);
   });
 });
